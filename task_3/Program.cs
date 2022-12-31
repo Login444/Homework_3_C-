75c5cdfd4f966a75ff9385d4b05881f2bca88999
Artemiy <birukovartemij@gmail.com>
2022-12-31 10:30:41 +0300

    решил задачу 3

---
+++ task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+int count = 1;
+
+while(count < N)
+{
+int num = Convert.ToInt32(Math.Pow(count, 3));
+Console.Write($"{num}, ");
+count++;
+}
+Console.Write($"{Math.Pow(N, 3)}");
